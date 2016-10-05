9d874cdc3baf4984b78e371b4d115f707e47c131
nextacademy <yithanglee@gmail.com>
2016-10-06 00:16:55 +0800

    finsihed the password validation

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,24 +1,32 @@
 get '/' do
-
   erb :"static/index"
 end
-
 
 post '/signup' do
 	@user = User.new(params[:user])
 	# byebug
 
 	if @user.save
-
 		redirect '/'
-
 	else
 		@errors = @user.errors.full_messages
-
-
   erb :"static/index"
 	# end for if
 	end
-
 # end for post
+end
+
+post '/login' do
+
+# byebug
+
+@user = User.oauthenticate(params["user"]["email"],params["user"]["password"])
+
+  erb :"static/index"
+
+end
+
+
+post '/logout' do
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,23 @@ validates :name, uniqueness: true, presence: true
 validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, presence: true
 
 
+	def self.oauthenticate(oemail, opassword)
+byebug
+a = self.find_by(email: oemail)
+
+		if oemail == a.email
+			if self.find_by(email: oemail).try(:authenticate, opassword)
+				return "sucess!"
+			else
+				return "wrong password!"
+			# end for if authenticate
+			end
+			
+		else
+			return "no such combination for email and password!"
+			# end for if email == self
+		end
+# end for authenticate method
+	end
+# end for class
 end
