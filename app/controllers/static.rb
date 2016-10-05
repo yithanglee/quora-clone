c8d88aa9346fd3372336282946bba73ea47b8d33
nextacademy <yithanglee@gmail.com>
2016-10-06 05:14:02 +0800

    delete questions

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -13,12 +13,12 @@ post '/signup' do
   erb :"static/index"
 	# end for if
 	end
-# end for post
+	# end for post
 end
 
 post '/login' do
 
-@user_login = User.oauthenticate(params["user"]["email"],params["user"]["password"])
+	@user_login = User.oauthenticate(params["user"]["email"],params["user"]["password"])
 	if @user_login == "success!"
 	@user_login = User.find_by(email: params["user"]["email"])
 	session[:user_id] = @user_login.id
@@ -27,27 +27,51 @@ post '/login' do
 	@user_login
   erb :"static/index"
 	end
-
 end
 
 get '/homepage' do
+	@questions = Question.all
 
-erb :"static/homepage"
-
+	erb :"static/homepage"
 end
-
 
 post '/logout' do
 
-session[:user_id] = nil
-
-redirect '/'
-
+	session[:user_id] = nil
+	redirect '/'
 end
 
-
 get '/users/:id/edit' do
+	erb :"static/user_profile"
+end
 
-erb :"static/user_profile"
+get '/questions' do
+	
+	@list_question = current_user.questions
+	erb :"questions/index"
+end
 
+post '/questions' do
+	@question = Question.new(user_id:current_user.id, question:params["abc"][":question"])
+
+	if @question.save
+	 redirect '/homepage'
+	else
+	# end for if
+	end
+	# end for post question
+end
+
+delete '/questions/:question_id' do
+	byebug
+	@question = Question.find(params[:question_id])
+	@question.destroy
+	redirect '/questions'
+	#end for delete
+end
+
+patch '/questions/:question_id' do
+	@question = Question.find_by(params[:question_id])
+	@question.update(params[:question])
+	#end for patch
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
 	belongs_to :user
 	has_many :answers
 
+validates :question, presence: true
+
 end
